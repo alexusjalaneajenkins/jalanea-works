7d3df218a250145dc8d03a498c642bea4df9bba1
Alexus Jalanea Jenkins <alexxusjenkins91@gmail.com>
2025-12-17 13:21:06 -0500

    Fix build configs and enable auto-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -17,7 +21,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': resolve(__dirname, '.'),
       }
     }
   };
